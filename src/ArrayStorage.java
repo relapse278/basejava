35f57b82b8f13d4fc4b568afa775a027726f2862
Ruslan <r.schewtschenko@mail.ru>
2024-02-08 13:46:19 +0300

    Altered the ArrayStorage class due to the comments of the mentor: - changed the final constant into final constant - deleted the redundant initialization of an int variable with 0 - used only arrays instead of collections.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,21 +1,18 @@
 import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-import static java.util.Arrays.stream;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    final int MAX_SIZE = 10000;
+    final static int MAX_SIZE = 10000;
     Resume[] storage = new Resume[MAX_SIZE];
-    int size = 0;
+    int size;
 
     void clear() {
         Arrays.fill(storage, 0, size, null);
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
@@ -25,24 +22,21 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        return stream(storage)
-                .filter(x -> x.toString().equals(uuid))
-                .findFirst()
-                .orElse(new Resume("No such resume found!"));
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return storage[i];
+            }
+        }
+
+        return new Resume("No resume with uuid " + uuid + " found!");
     }
 
     void delete(String uuid) {
-        List<Resume> storageList = Arrays.asList(storage);
-
-        int index = IntStream.range(0, size)
-                .filter(i -> Objects.equals(storageList.get(i).toString(), uuid))
-                .findFirst()
-                .orElse(-1);
-
-        if (index != -1) {
-            var resumeStream = Stream.concat(Arrays.stream(storage).limit(index), Arrays.stream(storage).skip(index + 1));
-            storage = resumeStream.toArray(Resume[]::new);
-            size--;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                storage[i] = storage[size - 1];
+                storage[--size] = null;
+            }
         }
     }
 
@@ -50,7 +44,9 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.stream(storage).limit(size).toArray(Resume[]::new);
+        Resume[] temp = new Resume[size];
+        System.arraycopy(storage, 0, temp, 0, size);
+        return temp;
     }
 
     int size() {
