56fb6d4fe7edfd48c49e3d8d270c304186bc3a20
Руслан Шевченко <r.schewtschenko@mail.ru>
2024-05-08 20:48:35 +0700

    HW 05 (Part 2) - Altered storage type in the MapStorage class from HashMap to LinkedHashMap in order to provide the sequence of the input.

--- src/com/urise/webapp/storage/MapStorage.java
+++ src/com/urise/webapp/storage/MapStorage.java
@@ -2,11 +2,11 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class MapStorage extends AbstractStorage {
-    private Map<String, Resume> map = new HashMap<>();
+    private final Map<String, Resume> map = new LinkedHashMap<>();
 
     @Override
     protected boolean isExisting(Object key) {
