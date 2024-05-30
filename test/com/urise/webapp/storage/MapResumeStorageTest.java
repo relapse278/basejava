864deb20d114986c51c88f85b68db3f6956df6f4
Руслан Шевченко <r.schewtschenko@mail.ru>
2024-05-31 00:00:40 +0700

    HW 06 - temporary commit
    1) Renamed the MapStorageTest into MapUuidStorageTest.
    
    2) Added the MapResumeStorageTest class.

---
+++ test/com/urise/webapp/storage/MapResumeStorageTest.java
@@ -0,0 +1,7 @@
+package com.urise.webapp.storage;
+
+public class MapResumeStorageTest extends AbstractStorageTest {
+    public MapResumeStorageTest(Storage storage) {
+        super(new MapResumeStorage());
+    }
+}
